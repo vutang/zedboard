fdaf1905011b8212f7bcf6bdcbcfc166721ddebd
Vu Tang <tangvubk@gmail.com>
2018-11-10 10:13:25 +0700

    ldd: add device driver file to /proc/devices by cinit & cadd

--- components/linux-kernel/xlnx-4.0-mod/drivers/vux_ldd/scull/scull_main.c
+++ components/linux-kernel/xlnx-4.0-mod/drivers/vux_ldd/scull/scull_main.c
@@ -1,15 +1,49 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>		/* everything... */
+#include <linux/cdev.h>
 
 int scull_major =   0;
 int scull_minor =   0;
 int scull_nr_devs = 4;	/* number of bare scull devices */
 
+struct cdev scull_cdev;	  /* Char device structure		*/
+
+int scull_open(struct inode *inode, struct file *filp) {
+	printk("skull_open\n");
+	return 0;
+}
+
+int scull_release(struct inode *inode, struct file *filp) {
+	printk("skull_release\n");
+	return 0;
+}
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+                loff_t *f_pos) {
+	printk("scull_read: %d\n", count);
+	return 0;
+}
+
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+                loff_t *f_pos){
+	printk("scull_write\n");
+	return count;
+}
+struct file_operations scull_fops = {
+	.owner =    THIS_MODULE,
+	// .llseek =   scull_llseek,
+	.read =     scull_read,
+	.write =    scull_write,
+	// .ioctl =    scull_ioctl,
+	.open =     scull_open,
+	.release =  scull_release,
+};
+
 int scull_init_module(void) {
 	printk("[vux] scull_init_module\n");
 
-	int result, i;
+	int result, i, err;
 	dev_t dev = 0;
 
 	if (scull_major) {
@@ -17,6 +51,7 @@ int scull_init_module(void) {
 		result = register_chrdev_region(dev, scull_nr_devs, "scull");
 	} 
 	else {
+		printk("Dynamic Allocate major\n");
 		result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs,
 				"scull");
 		scull_major = MAJOR(dev);
@@ -28,6 +63,14 @@ int scull_init_module(void) {
 		printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
 		return result;
 	}
+
+	dev_t devno = MKDEV(scull_major, 0);
+
+	cdev_init(&scull_cdev, &scull_fops);
+	err = cdev_add (&scull_cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_NOTICE "Error %d adding scull", err);
 	return 0;
 }
 
